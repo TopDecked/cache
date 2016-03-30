196690f146b412c41ed1e29f03e6b71072eb0315
Sergio Moura <sergio@moura.us>
2016-03-30 08:20:20 -0300

    Added delete functionality. Added compression support.

--- index.js
+++ index.js
@@ -3,17 +3,29 @@
 /**
  * Very simple disk cache module
  * @param cachedir String with cache dir. if none is given, <appdir>/cache is used.
- * @param debug Function (optional) to pass debug data to. You may use `console.log`.
+ * @param options Object with the following valid options
+ * * debug Function (optional) to pass debug data to. You may use `console.log`.
+ * * compress Boolean (optional) to compress or not to compress (default: false)
  *
  * adapted from: https://github.com/mostlygeek/Node-Simple-Cache
  */
-module.exports = function(cachedir, debug) {
+module.exports = function(cachedir, options) {
 	var fs = require('fs');
 	var path = require('path');
 	var promise = require('promise');
 	var crypto = require('crypto');
 
-	debug = debug || function() {};
+	var zlib;
+	var debug;
+	var compress;
+
+	if (options.debug) {
+		debug = options.debug || function() {};
+	}
+	if (options.compress === true) {
+		zlib = require('zlib');
+	}
+
 	cachedir = cachedir || path.join(process.cwd, 'cache');
 	var context = null;
 
@@ -29,22 +41,34 @@ module.exports = function(cachedir, debug) {
 		var ret = new promise();
 		var file = cachenameFunc(key);
 
+		var compressPromise = new promise();
+		compressPromise.done(ret);
+
 		fs.readFile(file, 'utf8', function(err, data) {
 			if (err) {
 				if (!fillFn) {
-					ret.resolve(null);
+					ret.reject('no fill function');
 					return;
 				}
 				fillFn.call(context, function(results) {
 					writeFunc(key, results);
 					ret.resolve(results);
 				});
-			} else {
+			}
+			else {
 				/**
 				 * Resolve promise immediately with the 
 				 * data from the cache. 
 				 */
-				ret.resolve(data);
+				if (options.compress === true) {
+					zlib.gunzip(data, function(err, data) {
+						if (err) compressPromise.reject(err);
+						else compressPromise.resolve(data);
+					});
+				}
+				else {
+					compressPromise.resolve(data);
+				}
 			}
 		});
 
@@ -60,14 +84,59 @@ module.exports = function(cachedir, debug) {
 		var file = cachenameFunc(key);
 		var ret = new promise();
 
-		fs.writeFile(file, data, function(err) {
-			if (err) {
-				debug("Cache write error: " + err);
-			}
-			ret.resolve();
+		var compressPromise = new promise();
+
+		compressPromise.done(function(err, data) {
+			if (err) { ret.reject(err); return; }
+
+			fs.writeFile(file, data, function(err) {
+				if (err) {
+					debug("Cache write error: " + err);
+				}
+				ret.resolve();
+			});
 		});
 
-		return(ret);
+		if (options.compress === true) {
+			actualData = zlib.gzip(
+				data,
+				function(err, data) {
+					if (err) compressPromise.reject(err, data);
+					else compressPromise.resolve(data);
+				}
+			);
+		}
+		else {
+			compressPromise.resolve(data);
+		}
+
+		return (ret);
+	};
+
+	/**
+	 * Deletes the given key from the cache
+	 * @param key String value to delete from cache
+	 * @return promise
+	 */
+	var deleteFunc = function(key) {
+		var file = cachenameFunc(key);
+		var ret = new promise();
+
+		fs.stat(file, function(err, stat) {
+			if (err == null) {
+				fs.unlink(file, function() {
+					promise.resolve();
+				});
+			}
+			else if (err.code == 'ENOENT') {
+				promise.resolve();
+			}
+			else {
+				promise.reject('Error erasing file: ' + err.code);
+			}
+		});
+
+		return (ret);
 	};
 
 	/**
@@ -85,6 +154,7 @@ module.exports = function(cachedir, debug) {
 	context = {
 		get: getFunc,
 		write: writeFunc,
+		delete: deleteFunc,
 		cachname: cachenameFunc
 	};
 
